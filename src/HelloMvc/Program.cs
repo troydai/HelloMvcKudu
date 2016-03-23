4d79ba93a831a7c8a5aeeaf21ea495a49c34b5ec
Troy Dai <troy.dai@outlook.com>
2016-03-23 12:07:13 -0700

    v0

---
+++ src/HelloMvc/Program.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace HelloMvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseServer("Microsoft.AspNetCore.Server.Kestrel")
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseDefaultConfiguration(args)
+                        .UseIISPlatformHandlerUrl()
+                        .UseStartup<Startup>()
+                        .Build();
+
+            host.Run();
+        }
+    }
+}
