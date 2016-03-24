5a7d2c1f6d3697eb70730a7c3979537e8a567ac4
Troy Dai <troy.dai@outlook.com>
2016-03-24 16:24:25 -0700

    Publish project

--- deploy.ps1
+++ deploy.ps1
@@ -13,3 +13,7 @@ $sdk_folder = (ls -Directory .dotnet\sdk\ | select -first 1).FullName
 
 # Restore packages
 & "$sdk_folder\corehost.exe" $sdk_folder\dotnet.dll restore
+
+# Publish
+$output = "$env:DEPLOYMENT_TARGET\.."
+& "$sdk_folder\corehost.exe" $sdk_folder\dotnet.dll publish .\src\HelloMvc\project.json -o $output
