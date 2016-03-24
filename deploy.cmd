c2be6b7a0683e08b5bb8ab3c73ea5a15ec4a58bf
Troy Dai <troy.dai@outlook.com>
2016-03-24 15:04:26 -0700

    Disable trace

--- deploy.cmd
+++ deploy.cmd
@@ -4,6 +4,6 @@ rem Deployment script for a CLI based rc2 ASP.NET MVC web applicaiton
 
 echo "In deploy.cmd"
 
-set COREHOST_TRACE=1
+rem set COREHOST_TRACE=1
 
 powershell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "%~dp0deploy.ps1 %*; exit $LastExitCode;"
