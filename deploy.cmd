0950beaa48027a47496bd3426935cf0716f7e9be
Troy Dai <troy.dai@outlook.com>
2016-03-24 13:58:17 -0700

    Install dotnet and find its version

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,9 @@
+rem Deployment script for a CLI based rc2 ASP.NET MVC web applicaiton
+
+@echo off
+
 echo "In deploy.cmd"
 
-powershell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "%~dp0deploy.ps1 %*; exit $LastExitCode;"
+set COREHOST_TRACE=1
 
+powershell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "%~dp0deploy.ps1 %*; exit $LastExitCode;"
--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,7 @@
+# Deployment script for CLI based rc2 ASP.NET MVC Application
+
 $ProgressPreference="SilentlyContinue"
 
-./install.ps1 beta
+.\install.ps1 -Channel beta -InstallDir .dotnet
+
+.\.dotnet\dotnet.exe --version
